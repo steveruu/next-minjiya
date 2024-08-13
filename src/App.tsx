8a78b5dc75612a63b958cda892187b9b3bc8495f
st:prchal <steveruu@stvr.cz>
2024-08-14 00:11:44 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <picture>
+        <source srcSet="/mt-day.svg" type="image/svg+xml" media="(prefers-color-scheme: light)" />
+        <img src="mt.svg" />
+      </picture>
+      <h1>みんじや</h1>
+      <code>
+        minjiya
+      </code>
+    </>
+  )
+}
+
+export default App;
