58963a996d1b278cc5856589c1a9c3143a584798
st:prchal <steveruu@stvr.cz>
2024-08-20 01:29:16 +0200

    branding site at /#/branding/

---
+++ src/Branding.tsx
@@ -0,0 +1,14 @@
+const Branding = () => {
+    return (
+        <div className="branding-site">
+            <figure className="branding">
+                <img src="/images/AtlasMinjiya.png" alt="branding" />
+            </figure>
+            <figure className="branding">
+                <img src="/images/AtlasMinjiyaFull.png" alt="branding" />
+            </figure>
+        </div>
+    )
+}
+
+export default Branding
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import NotFound from './NotFound'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Branding from './Branding'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/branding" element={<Branding />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
