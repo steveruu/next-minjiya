3ac0892bb2f2fce59be2bda706bf604995acf65f
st:prchal <steveruu@stvr.cz>
2024-09-20 19:40:07 +0200

    styles fix; font removals

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <div className="flex flex-col h-screen justify-center items-center gap-6 w-screen">
-            <h1 className="text-5xl font-[Stick] leading-[1.1]">404</h1>
+            <h1 className="text-5xl font-[Stick] leading-[1.1] text-white">404</h1>
             <code className="text-2xl tracking-widest text-[#999]">
                 page not found
             </code>
