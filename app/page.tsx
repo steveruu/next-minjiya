c3d80160a86bb3a49d14c9ea941cbd58243d12d8
st:prchal <steveruu@stvr.cz>
2024-09-20 19:10:52 +0200

    -light mode mediaquery; design consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main className="mx-auto">
-      <picture>
-        <source srcSet="/mt-day.svg" type="image/svg+xml" media="(prefers-color-scheme: light)" />
-        <img src="mt.svg" className="inline" alt="minjiya" />
-      </picture>
-      <h1 className="text-5xl my-8 font-[Stick] leading-[1.1]">みんじや</h1>
+      <Image src="mt.svg" className="inline" alt="minjiya" width={275} height={148} />
+      <h1 className="text-5xl my-8 font-[Stick] leading-[1.1] text-white">みんじや</h1>
       <code className="text-2xl tracking-widest text-[#999]">
         shaping the future
       </code>
