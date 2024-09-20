99b2fb347bdd0bdede4536e381d49a7917b1cb33
st:prchal <steveruu@stvr.cz>
2024-09-20 19:01:22 +0200

    OG & Twitter metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,33 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "minjiya",
   description: 'shaping the future',
+  openGraph: {
+    type: 'website',
+    url: "https://minjiya.com",
+    title: "minjiya",
+    description: 'shaping the future',
+    images: [
+      {
+        url: "https://minjiya.com/mt.png",
+        width: 800,
+        height: 600,
+        alt: "minjiya",
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "minjiya",
+    description: 'shaping the future',
+    images: [
+      {
+        url: "https://minjiya.com/mt.png",
+        width: 800,
+        height: 600,
+        alt: "minjiya",
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
