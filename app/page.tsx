a10dc8bbb0566bd1dbb009928e9d8f721cc7ee9a
steveruu <steveruu@stvr.cz>
2024-09-27 22:08:33 +0200

    nikdo nas neucil jak delat SEO, tak to delam podle Next.js dokumentace

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <main className="mx-auto">
-      <Image src="mt.svg" className="inline" alt="minjiya" width={275} height={148} />
-      <h1 className="text-5xl my-8 font-[Stick] leading-[1.1] text-white">みんじや</h1>
-      <code className="text-2xl tracking-widest text-[#999]">
-        shaping the future
-      </code>
-    </main>
-  );
+    return (
+        <>
+            <Head>
+                <meta name="robots" content="all" />
+            </Head>
+            <main className="mx-auto">
+                <Image
+                    src="mt.svg"
+                    className="inline"
+                    alt="minjiya"
+                    width={275}
+                    height={148}
+                />
+                <h1 className="text-5xl my-8 font-[Stick] leading-[1.1] text-white">
+                    みんじや
+                </h1>
+                <code className="text-2xl tracking-widest text-[#999]">
+                    shaping the future
+                </code>
+            </main>
+        </>
+    );
 }
