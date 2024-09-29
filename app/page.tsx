3edba9cb1f37498b58d78ce77482a6cbdc3b265b
st:prchal <steveruu@stvr.cz>
2024-09-29 17:48:59 +0200

    mobile responsivity edits

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen w-full flex-col bg-[#161729]">
+        <div className="flex min-h-screen w-screen flex-col bg-[#161729]">
             <Header />
-            <div className="flex flex-col md:flex-row">
-                <div className="mx-auto my-12">
+            <div className="flex flex-col xl:flex-row items-center">
+                <div className="flex flex-col items-center mx-auto my-10 md:my-20 w-[50%] max-w-md md:py-8">
                     <Image
                         src="mt.svg"
                         className="inline"
@@ -17,13 +17,44 @@ export default function Home() {
                     <h1 className="text-3xl mt-8 mb-2 leading-[1.1] font-[Mona Sans] font-bold text-white">
                         minjiya
                     </h1>
-                    <code className="text-2xl tracking-widest text-neutral-400">
+                    <h3 className="md:text-2xl tracking-wider text-neutral-400 font-[Rubik]">
                         shaping the future
-                    </code>
+                    </h3>
                 </div>
-                <div>
-                    <pre>
-
+                <div className="w-[100%] md:w-[50%] flex justify-center">
+                    <pre className="mx-4 overflow-y-auto md:overflow-visible md:flex-nowrap bg-neutral-950 py-2 px-4 rounded-xl">
+                        <code className="md:block text-xs md:text-base text-white ">
+                            <span style={{ color: "#569CD6" }}>import</span> React <span style={{ color: "#569CD6" }}>from</span> <span style={{ color: "#CE9178" }}>'react'</span>;
+                            <br />
+                            <span style={{ color: "#569CD6" }}>import</span> Head <span style={{ color: "#569CD6" }}>from</span> <span style={{ color: "#CE9178" }}>'next/head'</span>;
+                            <br />
+                            <br />
+                            <span style={{ color: "#569CD6" }}>export default function</span> <span style={{ color: "#ffffff" }}>Home</span>() {"{"}
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#569CD6" }}>return</span> (
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;div&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;Head&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;title&gt;</span><span style={{ color: "#ffffff" }}>My Next.js App</span><span style={{ color: "#CC5656" }}>&lt;/title&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;/Head&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;main&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;h1&gt;</span><span style={{ color: "#ffffff" }}>Welcome to My Next.js App</span><span style={{ color: "#CC5656" }}>&lt;/h1&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;p&gt;</span><span style={{ color: "#ffffff" }}>This is a sample Next.js application.</span><span style={{ color: "#CC5656" }}>&lt;/p&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;/main&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{ color: "#CC5656" }}>&lt;/div&gt;</span>
+                            <br />
+                            &nbsp;&nbsp;&nbsp;&nbsp;);
+                            <br />
+                            {"}"}
+                        </code>
                     </pre>
                 </div>
             </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <header className="sticky top-0 flex h-16 items-center gap-4 border-b border-b-zinc-800 bg-background px-4 md:px-6 bg-zinc-950 text-slate-200">
+        <header className="top-0 flex h-16 items-center gap-4 border-b border-b-zinc-800 bg-background px-4 md:px-6 bg-zinc-950 text-slate-200">
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ">
                 <Link
                     href="#"
