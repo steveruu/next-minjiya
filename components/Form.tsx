c91824c5e690b0fc4ed503057814cc5b06c951c6
st:prchal <steveruu@stvr.cz>
2024-10-06 21:59:57 +0200

    FORM handling

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,16 +1,37 @@
+"use client"
+import { formHandle } from "@/lib/route"
+import { useFormState } from 'react-dom';
+import { toast, Toaster } from "react-hot-toast";
+
 const Form = () => {
+    const [state, formAction] = useFormState(
+        (prevState: any, formData: FormData) => formHandle(prevState, formData),
+        { status: 418, message: "🫖" }
+    );
+
+    if (state.status === 200) {
+        toast.success(state.message);
+    } else if (state.status == 400 || state.status == 429) {
+        toast.error(state.message);
+    }
+
     return (
         <>
-            <form >
+            <Toaster position="bottom-center" />
+            <form action={formAction}>
                 <div className="flex flex-col gap-4 pt-8 font-rubik ">
                     <input
                         type="text"
+                        id="userFirstName"
+                        name="userFirstName"
                         placeholder="Jméno"
                         className="p-2 border-2 border-gray-300 rounded-md"
                         required
                     />
                     <input
                         type="email"
+                        id="email"
+                        name="email"
                         placeholder="Email"
                         className="p-2 border-2 border-gray-300 rounded-md"
                         required
@@ -19,15 +40,17 @@ const Form = () => {
                         placeholder="Zpráva"
                         className="p-2 border-2 border-gray-300 rounded-md max-h-72"
                         required
+                        id="text"
+                        name="text"
                     />
                     <button
                         type="submit"
                         className="p-2 border-purpur border-2 text-purpur rounded-md font-semibold "
                     >
-                        Odeslat
+                        Odeslat PŘIDAT RECAPTCHA TODO
                     </button>
-                </div>
-            </form>
+                </div >
+            </form >
         </>
     )
 }
--- lib/route.ts
+++ lib/route.ts
@@ -4,14 +4,25 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request, res: Response) {
-    const { email, userFirstName, text } = await req.json();
+const isInvalid = (text: FormDataEntryValue, charLimit: number = 100) => {
+    return (
+        !text ||
+        text.toString().trim() == "" ||
+        text.toString().length < 4 ||
+        text.toString().length > charLimit
+    );
+};
 
+export async function resend_email_send(formD: {
+    email: FormDataEntryValue;
+    userFirstName: FormDataEntryValue;
+    text: FormDataEntryValue;
+}) {
     const { data, error } = await resend.emails.send({
         from: "minjiya-form <form@minjiya.com>",
         to: ["inquiries@minjiya.com"],
         subject: "Nový přijatý formulář",
-        html: `<h1>Přišla vám nová zpráva od ${email}</h1><h3>${userFirstName}</h3><p>${text}</p>`,
+        html: `<h1>Přišla vám nová zpráva od ${formD.email}</h1><h3>${formD.userFirstName}</h3><p>${formD.text}</p>`,
     });
 
     if (error) {
@@ -19,4 +30,45 @@ export async function POST(req: Request, res: Response) {
     }
 
     return Response.json({ data });
+}
+
+let requestCount = 0;
+const RATE_LIMIT = 3;
+const TIME_WINDOW = 60 * 1000; // 3 za minutu
+let firstRequestTime = Date.now();
+
+export async function formHandle(prevState: any, formD: FormData) {
+    const currentTime = Date.now();
+
+    if (currentTime - firstRequestTime > TIME_WINDOW) {
+        firstRequestTime = currentTime;
+        requestCount = 0;
+    }
+
+    requestCount++;
+
+    if (requestCount > RATE_LIMIT) {
+        return {
+            status: 429,
+            message: "Too many requests. Please try again later.",
+        };
+    }
+
+    const data = {
+        email: formD.get("email") ?? "",
+        userFirstName: formD.get("userFirstName") ?? "",
+        text: formD.get("text") ?? "",
+    };
+
+    if (
+        isInvalid(data.email, 40) ||
+        isInvalid(data.userFirstName, 40) ||
+        isInvalid(data.text, 1500) ||
+        !data.email.toString().includes("@")
+    ) {
+        return { status: 400, message: "Vyplňte všechna pole správně!" };
+    } else {
+        // await resend_email_send(data)
+        return { status: 200, message: "Zpráva byla úspěšně odeslána!" };
+    }
 }
