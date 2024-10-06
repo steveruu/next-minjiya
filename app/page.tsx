6e7a3c1a9c0655826148680f212de897a37aeaf0
st:prchal <steveruu@stvr.cz>
2024-10-06 14:08:47 +0200

    gitignore update

--- app/page.tsx
+++ app/page.tsx
@@ -54,11 +54,10 @@ export default function Home() {
                 >
                     our references
                 </p>
-                <div className="flex flex-row flex-wrap gap-4 mt-10 justify-center items-center max-w-[1024px] mx-auto">
+                <div className="flex flex-row flex-wrap gap-4 mt-10 justify-center items-center max-w-[80%] mx-auto">
                     <QuoteCard author="Dollar Prync" quote="Sleduj malá Minjiyo, sleduj teď" authorImage="https://eventigo.s3-central.vshosting.cloud/production/artist/dollar-prync-generace-rap-kopie-jpg-zjekmo.jpeg" />
                     <QuoteCard author="Sergei Barracuda" quote="Budu v NHčku jak Minjiya chcípat na bolesti zad" authorImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBur3Y-4p-Z407ixxZWIvXa6sh6cRSMKsDG47gYL_PnnEZukeo" />
                     <QuoteCard author="Dollar Prync" quote="Pojď sem baby, dělej tou Minjiyou teď a tady" authorImage="https://eventigo.s3-central.vshosting.cloud/production/artist/dollar-prync-generace-rap-kopie-jpg-zjekmo.jpeg" />
-
                 </div>
             </div>
             <footer className="py-4 text-center text-neutral-400 bg-neutral-800">
