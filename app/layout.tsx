b23d6f8d1fb9b15db64c5a5b4e946506b1b563a6
steveruu <steveruu@stvr.cz>
2024-10-14 22:26:42 +0200

    Layout.tsx edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,54 @@
 import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "minjiya",
-  description: 'tvoříme webové a mobilní aplikace budoucnosti',
-  openGraph: {
-    type: 'website',
-    url: "https://minjiya.com",
     title: "minjiya",
-    description: 'tvoříme webové a mobilní aplikace budoucnosti',
-    siteName: "minjiya",
-    images: [
-      {
-        url: "https://minjiya.com/mt.png",
-        width: 800,
-        height: 600,
-        alt: "minjiya",
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: "minjiya",
-    description: 'tvoříme webové a mobilní aplikace budoucnosti',
-    images: [
-      {
-        url: "https://minjiya.com/mt.png",
-        width: 800,
-        height: 600,
-        alt: "minjiya",
-      },
-    ],
-  }
+    description: "Tvoříme webové a mobilní aplikace budoucnosti",
+    openGraph: {
+        type: "website",
+        url: "https://minjiya.com",
+        title: "minjiya",
+        description: "Tvoříme webové a mobilní aplikace budoucnosti",
+        siteName: "minjiya",
+        images: [
+            {
+                url: "https://minjiya.com/mt.png",
+                width: 800,
+                height: 600,
+                alt: "minjiya",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "minjiya",
+        description: "Tvoříme webové a mobilní aplikace budoucnosti",
+        images: [
+            {
+                url: "https://minjiya.com/mt.png",
+                width: 800,
+                height: 600,
+                alt: "minjiya",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="scroll-smooth">
-      <body
-        className={inter.className}
-      >
-        <Analytics />
-        {children}
-      </body>
-    </html >
-  );
+    return (
+        <html lang="en" className="scroll-smooth">
+            <body className={inter.className}>
+                <Analytics />
+                {children}
+            </body>
+        </html>
+    );
 }
