df05715acf31a2ce440ce38ddf00856e00182ca3
st:prchal <steveruu@stvr.cz>
2024-11-01 23:02:33 +0100

    SpeedInsights tests

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import "./globals.css";
 import { Inter } from 'next/font/google'
 
@@ -52,6 +54,7 @@ export default function RootLayout({
       >
         <Analytics />
         {children}
+        <SpeedInsights />
       </body>
     </html >
   );
