c4301e6c3150df4c0029ac034a3c54e2f7b158bd
st:prchal <steveruu@stvr.cz>
2025-02-28 15:35:40 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,8 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: ["/"],
-            disallow: ["/api", "/bahno"],
+            disallow: ["/api", "/newshyshow.php"],
         },
+        sitemap: "https://minjiya.com/sitemap.xml",
     };
 }
